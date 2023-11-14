c6bb493580f90251e9d0086729936e0cd287498d
tansimin <threeyear0629@163.com>
2023-11-14 13:35:40 +0800

    字典页面修复

--- src/views/system/dict/index.vue
+++ src/views/system/dict/index.vue
@@ -13,7 +13,7 @@
       <el-table-column label="排序" prop="orderNum" />
       <el-table-column label="状态" prop="status">
         <template #default="{ row }">
-          <span>{{ row.status ? '禁用' : '停用' }}</span>
+          <span>{{ row.status ? '启用' : '禁用' }}</span>
         </template>
       </el-table-column>
       <el-table-column label="备注" prop="remark" :show-overflow-tooltip="true" />
